47105ee692f2fa6b48390f490ddf94a58cec9dec
syncvi <ggobestie@gmail.com>
2022-10-16 16:12:40 +0200

    Version 2 changes

--- app/src/main/java/com/example/quizlet/MainActivity.java
+++ app/src/main/java/com/example/quizlet/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.quizlet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String QUIZ_TAG = "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "quizlet.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
     private TextView questionTextView;
+    private boolean answerWasShown;
     private int currentIndex = 0;
 
     private Question[] questions = new Question[]{
@@ -26,11 +36,17 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if( userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }
-        else{
-            resultMessageId = R.string.incorrect_answer;
+        else {
+            if (userAnswer == correctAnswer)
+            {
+                resultMessageId = R.string.correct_answer;
+            }
+            else
+                resultMessageId = R.string.incorrect_answer;
+
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
@@ -39,15 +55,34 @@ public class MainActivity extends AppCompatActivity {
         questionTextView.setText(questions[currentIndex].getQuestionId());
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return ;
+        }
+        if(requestCode == REQUEST_CODE_PROMPT){
+            if( data == null){
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onCreate");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState!=null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         Button trueButton = findViewById(R.id.true_button);
         Button falseButton = findViewById(R.id.false_button);
         Button nextButton = findViewById(R.id.next_button);
+        Button hintButton = findViewById(R.id.hint_button);
         questionTextView = findViewById(R.id.question_text_view);
 
         trueButton.setOnClickListener(new View.OnClickListener(){
@@ -67,9 +102,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 currentIndex = (currentIndex + 1 ) %questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
+        hintButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
         setNextQuestion();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, "The Activity-lifecycle method has been executed: onResume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Method has been executed: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
 }
